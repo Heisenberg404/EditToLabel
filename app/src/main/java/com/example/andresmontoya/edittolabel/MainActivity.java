89027ea59e5322c5aa05fc21392b55422de7bfef
amontoya49 <andresmhkun@gmail.com>
2016-10-06 10:50:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/andresmontoya/edittolabel/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.andresmontoya.edittolabel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText)findViewById(R.id.edit);
+        textView = (TextView)findViewById(R.id.texto);
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                textView.setText("Hello: "+s.toString());
+            }
+        });
+    }
+
+}
